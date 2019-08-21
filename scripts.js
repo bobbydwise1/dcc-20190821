3ceb49e1abd3bbf8d79d21569dcade79de286fae
Robert Lee <bobbydwise1@hotmail.com>
2019-08-21 16:49:40 -0700

    Add array generator function.

--- scripts.js
+++ scripts.js
@@ -10,7 +10,28 @@ For example, given [6, 1, 3, 3, 3, 6, 6], return 1. Given [13, 19, 13, 13], retu
 Do this in O(N) time and O(1) space.
 */
 
+const makeRandomNumber = (min,max) => {
+  return Math.round(Math.random()*(max-min)+min);
+}
 
+const makeArray = () => {
+  let output = [];
+  let upper = makeRandomNumber(2,10);
+  let temp;
+  let i = 0;
+  while  (i<upper) {
+    temp = makeRandomNumber(1,99)
+    if (output.find(function(element) {return element == temp}) != temp) {
+      output.push(temp);
+      i++
+      console.log('i = ', i, ' , temp = ', temp)
+    }
+  }
+  return output;
+}
+
+let test1 = makeArray();
+console.log(test1)
 
 $(document).ready(function() {
   $('#button1').click(function(){
