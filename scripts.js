91913a98375e46e0ea364fc6384a4ec2b5e3438a
Robert Lee <bobbydwise1@hotmail.com>
2019-08-21 20:39:19 -0700

    Add more generator functions.

--- scripts.js
+++ scripts.js
@@ -14,24 +14,73 @@ const makeRandomNumber = (min,max) => {
   return Math.round(Math.random()*(max-min)+min);
 }
 
+const findInArray = (yourArray,singleEle) => {
+  return yourArray.find(function(element) {
+    return element == singleEle
+  });
+}
+
+const filterArray = (yourArray,getRidof) => {
+  return yourArray.filter(function(value, index, arr) {
+    return value != getRidof;
+  })
+}
+
+const insertIntoMiddle = (someArray,value,index) => {
+  if ((index > someArray.length-1)||(index < 0)) {
+    return null;
+  } else if (index == 0) {
+    return someArray.slice().shift(value)
+  } else if (index == someArray.length-1) {
+    return someArray.slice().push(value)
+  } else {
+    let left = someArray.slice(0,index);
+    let right = someArray.slice(index,someArray.length);
+    return left.concat([value], right)
+  }
+}
+
 const makeArray = () => {
   let output = [];
-  let upper = makeRandomNumber(2,10);
+  let upper = makeRandomNumber(2,9);
   let temp;
   let i = 0;
   while  (i<upper) {
     temp = makeRandomNumber(1,99)
-    if (output.find(function(element) {return element == temp}) != temp) {
+    if (findInArray(output,temp) != temp) {
+      output.push(temp);
+      output.push(temp);
       output.push(temp);
       i++
       console.log('i = ', i, ' , temp = ', temp)
     }
   }
+  temp = makeRandomNumber(1,99)
+  if (findInArray(output,temp) != temp) {
+    output.push(temp);}
+  return output;
+}
+
+const shuffleArray = (yourArray) => {
+  let picked;
+  let output = [];
+  let copy = yourArray.slice();
+  console.log('length = ', yourArray.length, copy.length)
+  for (let i = 0; i < copy.length; i++) {
+    picked = Math.floor(Math.random()*copy.length)
+    console.log(copy[picked])
+    output.push(copy[picked])
+    copy.splice(picked,1)
+    //console.log('output = ', output)
+    //console.log('copy = ', copy)
+  }
   return output;
 }
 
 let test1 = makeArray();
-console.log(test1)
+console.log('test1 = ', test1)
+let test2 = shuffleArray(test1);
+console.log('test2 = ', test2)
 
 $(document).ready(function() {
   $('#button1').click(function(){
